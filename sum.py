0f2ee4f0e05f5736045896a012090dadca97f74b
Abdul <abdulkareem6190@gmail.com>
2021-10-13 10:26:19 +0530

    second version of sum

--- sum.py
+++ sum.py
@@ -1,6 +1,7 @@
 a = int(input('Enter the first number .. '))
 b = int(input('Enter the second number .. '))
+c = int(input('Enter the third number .. '))
 
-s = a + b
+s = a + b + c
 
-print(f'Sum of {a} and {b} is {s}')
+print(f'Sum of {a},{b} and {c} is {s}')
